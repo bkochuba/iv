a66d49f93ca3310ecb2313502f22a42ec5f82db9
bkochuba <71841777+bkochuba@users.noreply.github.com>
2022-10-28 07:27:32 -0400

    plotting data

---
+++ app.py
@@ -0,0 +1,65 @@
+from dash import Dash, dcc, html, Output ,Input
+from dataProvider import dteData_app
+import plotly.express as px
+
+
+#this was the default sheet
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+#app = Dash(__name__)
+
+app.title = "0 DTE Options Data | SpotGamma "
+
+server = app.server
+
+df = dteData_app()
+strikelist = df.current_strike.to_list()
+ivListCurrent = df.current_smvVol.to_list()
+ivListHist = df.hist_smvVol.to_list()
+callVolumeList = df.current_callVolume.to_list()
+putVolumeList = df.current_putVolume.to_list()
+
+
+app.layout = html.Div(
+
+         children = [
+             html.H2(children = 'SPX 0 DTE Metrics',),
+             html.P(
+            children="Skew & Volume for 0 DTE",
+            ),
+    dcc.Graph(
+        figure = {
+            "data": [
+
+                    {"x": df['current_strike'], "y": df['current_smvVol'], "type": "lines", "name":"0 DTE IV", "template":"plotly_dark"},
+                    {"x": df['current_strike'], "y": df['hist_smvVol'], "type": "lines", "name":"0 DTE IV yesterday", "template":"plotly_dark"},
+
+            ],
+            "layout": {"title": "0DTE IV"},
+        },
+
+    ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {"x": df['current_strike'],"y": df['current_callVolume'],"type": "bar", "name":"0DTE Call Volume", "color":"blue", "template":"plotly_dark"},
+                    {"x": df['current_strike'], "y": df['current_putVolume'], "type": "bar", "name": "0DTE Put Volume", "template":"plotly_dark"},
+
+                ],
+                "layout": {"title": "0DTE Volume"},
+            },
+
+        ),
+
+
+    ]
+)
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
---
+++ dataProvider.py
@@ -0,0 +1,79 @@
+
+import requests
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
+
+def dteData_app():
+
+    #define strike range
+    stock = 'SPX'
+    lows = 3750
+    highs = 3950
+
+    theexp = '2022-10-28'
+    theexp1 = '2022-10-28'
+    histdate = '202210271600'
+
+    #types: hist, comp, single
+    charttype = 'hist'
+
+    url = "https://api.orats.io/datav2/one-minute/strikes/chain?token=d63fc184-fa10-4b3e-9a15-ce756595897b&ticker=" + str(stock)
+    payload={}
+    headers={}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    df = pd.read_csv(StringIO(response.text), sep=',')
+
+
+    #get past data
+    if charttype=='hist':
+        hist_url = "https://api.orats.io/datav2/hist/one-minute/strikes/chain?token=d63fc184-fa10-4b3e-9a15-ce756595897b&ticker="+ str(stock)+ "&tradeDate="+str(histdate)
+    else:
+        hist_url = "https://api.orats.io/datav2/one-minute/strikes/chain?token=d63fc184-fa10-4b3e-9a15-ce756595897b&ticker=" + str(stock)
+    payload={}
+    headers={}
+    response = requests.request("GET", hist_url, headers=headers, data=payload)
+    df_hist = pd.read_csv(StringIO(response.text), sep=',')
+
+    #various filtering
+    df['sym'] = df.ticker.astype(str)+df.expirDate.astype(str)+df.strike.astype(str)
+    putVolumeAll = df.putVolume.sum()
+    callVolumeAll = df.callVolume.sum()
+    print(putVolumeAll+callVolumeAll)
+
+    df = df[df.expirDate == theexp]
+    callVolumeNext = df.callVolume.sum()
+    putVolumeNext = df.putVolume.sum()
+    callOINext = df.callOpenInterest.sum()
+    putOINext = df.putOpenInterest.sum()
+    print(callOINext+putOINext)
+    print(callVolumeNext+putVolumeNext)
+    callPct = round(callVolumeNext/callVolumeAll,0)*100
+    putPct = round(putVolumeNext/putVolumeAll,0)*100
+
+    df = df[(df.strike>=lows) &  (df.strike<=highs)]
+    df['putOpenInterest'] = df.putOpenInterest * -1
+    df['putVolume'] = df.putVolume * -1
+    df.to_csv('merged1.csv')
+
+    df_hist['sym'] = (df_hist.ticker.astype(str))+(df_hist.expirDate.astype(str))+df_hist.strike.astype(str)
+
+    df_hist = df_hist[df_hist.expirDate == theexp1]
+
+    df_hist = df_hist[(df_hist.strike>=lows) & (df_hist.strike<=highs)]
+    df_hist.to_csv('merged2.csv')
+    df_hist['putOpenInterest'] = df_hist.putOpenInterest * -1
+    df_hist['putVolume'] = df_hist.putVolume * -1
+
+    df.columns = 'current_' + df.columns
+    df_hist.columns = 'hist_' + df_hist.columns
+    df2 = df.merge(df_hist, left_on='current_sym', right_on='hist_sym')
+    df2['callOIchg'] = df2.hist_callOpenInterest - df2.current_callOpenInterest
+    df2['putOIchg'] = df2.hist_putOpenInterest - df2.current_putOpenInterest
+    return(df2)
+
+
+
+
+
+
